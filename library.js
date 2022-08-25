bf71a69c138b1e9f98503f97cd2485f8b1d6e9ee
ghools <santad06@yahoo.com>
2022-08-24 23:27:46 -0400

    Add Book Button form functionality

--- library.js
+++ library.js
@@ -1,26 +1,28 @@
-const cardContainer = document.querySelector(".cardContainer")
-let myLibrary = []
-
+const cardContainer = document.querySelector(".cardContainer");
+let myLibrary = [];
 
 //Book constructor
-function Book(title, author, pages, read){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        return (`${this.title} by ${this.author}, ${pages} pages, ${read}`); //can use this. here if wanted, but not needed.
-    }
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${pages} pages, ${read}`; //can use this. here if wanted, but not needed.
+    };
 }
 
 //Making new Book Example
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkein", "295" , "Not Read");
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkein", "295", "Not Read");
 console.log(theHobbit.info());
 
-//Next on my Todo is this, make style change to flex...
-function bookForm(){
-    let popup = document.getElementsByClassName("formContainer").style.display = "flex"
-}
-function addBook() {
 
+function bookForm() {
+    document.querySelector(".formContainer").style.display = "flex";
 }
+
+function closeForm() {
+    document.querySelector(".formContainer").style.display = "none"
+}
+
+function addBook() {}
